557ebd82035a45de4e14071cb0def499abe3f611
paulogrillo <paulodigitalart@gmail.com>
2021-09-03 22:06:32 -0300

    Update exercises

--- java/AULAS/src/lista4/Exercise2.java
+++ java/AULAS/src/lista4/Exercise2.java
@@ -1,9 +1,42 @@
 package lista4;
 
+import java.util.Random;
+
 public class Exercise2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Método random. Joga o dado de fomra aleatória.
+		Random aleatorio = new Random();
+		
+		//Variaveis e vetores
+		int [] lancamento = new int [10];
+		int numeroDado, contador, numeroMaior=0, xNumeroMaior=0,y;
+		double media=0.00, x=0.0;
+		
+		//Loop
+		for(y=0; y<lancamento.length; y++) {
+			numeroDado = aleatorio.nextInt(6)+1;
+			lancamento[y] = numeroDado;
+			System.out.println("\n"+ (y+1)+"° Lançamento deu: "+ lancamento[y]);
+			
+			x = x + lancamento[y];
+			
+			if(numeroDado >= numeroMaior) {
+				numeroMaior = numeroDado;
+			}if(numeroMaior == lancamento[y]) {
+				xNumeroMaior++;
+			}else {
+				xNumeroMaior = 1;
+			}
+			numeroMaior = lancamento[y];
+		}
+		
+		media = x/10.0;
+		System.out.println("\n\nA média aritmética dos lançamentos foi de: "+media);
+		System.out.println("\nA quantidade que o número 6 saiu foi de: "+xNumeroMaior+" vezes. ");
+		
+		
 
 	}
 
