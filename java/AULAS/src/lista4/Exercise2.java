fbd6088786dc7897cb5609e99ef11370cac22940
paulogrillo <paulodigitalart@gmail.com>
2021-09-03 22:58:24 -0300

    Update exercise

--- java/AULAS/src/lista4/Exercise2.java
+++ java/AULAS/src/lista4/Exercise2.java
@@ -2,6 +2,13 @@ package lista4;
 
 import java.util.Random;
 
+/*
+ * 
+ * Um dado é lançado 10 vezes e o valor correspondente é anotado. 
+ * Faça um programa que gere um vetor com os lançamentos, escreva esse vetor. 
+ * A seguir determine e imprima a média aritmética dos lançamentos, contabilize 
+ * e apresente também quantas foram as ocorrências da maior pontuação.
+ */
 public class Exercise2 {
 
 	public static void main(String[] args) {
