7345a9ee4c7fd27c873b2a173aab8f7ea299cd92
paulogrillo <paulodigitalart@gmail.com>
2021-09-10 00:33:23 -0300

    Update G6BANK

---
+++ java/AULAS/BANCO/src/entities/ContaPoupanca.java
@@ -0,0 +1,36 @@
+package entities;
+
+public class ContaPoupanca extends Conta {
+	
+	//atributos
+	private int diaAniversario;
+	
+	//construtor
+	public ContaPoupanca(String cpf, int numero, int diaAniversario) {
+		super(cpf, numero);
+		this.diaAniversario = diaAniversario;
+	}
+	
+	//encapsulamento 
+	public int getDiaAniversario() {
+		return diaAniversario;
+	}
+
+	public void setDiaAniversario(int diaAniversario) {
+		this.diaAniversario = diaAniversario;
+	}
+	
+	
+	//Métodos
+	public void correcao(int diaInformado){
+		double valorCorrecao;
+		if(diaInformado == this.diaAniversario){
+			valorCorrecao = (super.getSaldo() * 0.005);
+			
+			super.credito(valorCorrecao);
+		}
+	}
+	
+	
+	
+}
