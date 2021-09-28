1654aa4de226aac5348b57467a300e50198bb449
paulogrillo <paulodigitalart@gmail.com>
2021-09-28 09:35:47 -0300

    update api

--- spring/blog/src/main/java/com/paulogrillo/blog/model/Tema.java
+++ spring/blog/src/main/java/com/paulogrillo/blog/model/Tema.java
@@ -2,10 +2,12 @@ package com.paulogrillo.blog.model;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
@@ -14,6 +16,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @Entity
 @Table(name = "tb_tema")
 public class Tema {
+
 	@Id	
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
@@ -21,6 +24,9 @@ public class Tema {
 	@NotNull
 	private String descricao;
 	
+	@OneToMany(mappedBy = "tema", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("tema")
+	private List<Postagens> postagem;
 
 	public long getId() {
 		return id;
@@ -38,6 +44,13 @@ public class Tema {
 		this.descricao = descricao;
 	}
 
+	public List<Postagens> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagens> postagem) {
+		this.postagem = postagem;
+	}
 
 
 	
